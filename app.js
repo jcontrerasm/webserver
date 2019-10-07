5eea41ecc0993af8739ac35b2ff78623e17ee5b5
Jc Contreras Meneses <jcontrerasmeneses@gmail.com>
2019-10-06 20:07:31 -0500

    feat(config): se realiza configuración de puerto (#1)

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { argv } from 'optimist';
 import { enviroment } from './config/enviroment';
 import { helpers } from './config/helpers';
 import { routes } from './config/routes';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || argv.port || 3000;
 
 enviroment(app, express);
 helpers(app);
