c278ca31ad07ea1a74b5a60d9b13188da868c0c5
Juan Contreras <jcontrerasmeneses@gmail.com>
2019-10-20 22:31:05 -0500

    refator(services): se realiza integracion con gcp

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-import express from 'express';
-import { argv } from 'optimist';
-import { enviroment } from './config/enviroment';
-import { helpers } from './config/helpers';
-import { routes } from './config/routes';
+// import express from 'express';
+// import { argv } from 'optimist';
+// import { enviroment } from './config/enviroment';
+// import { helpers } from './config/helpers';
+// import { routes } from './config/routes';
+
+// const app = express();
+// const port = process.env.PORT || argv.port || 3000;
+
+// enviroment(app, express);
+// helpers(app);
+// routes(app);
+
+// app.listen(port, () => console.log(
+//   `Node.js server is listening at http://localhost:${port}/`
+// ));
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || argv.port || 3000;
+const port = process.env.PORT || 3000;
 
-enviroment(app, express);
-helpers(app);
-routes(app);
+app.use(express.static(path.join(__dirname, './public')));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    page: 'Home'
+  });
+});
+
+app.get('/entregar-reporte', (req, res) => {
+  console.log('entrega de reportes...');
+  res.render('contact', {
+    page: 'Contacto'
+  });
+});
 
 app.listen(port, () => console.log(
   `Node.js server is listening at http://localhost:${port}/`
